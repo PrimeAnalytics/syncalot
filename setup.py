5ff7b998bd3491b0e71fabe4cc044f0e7795e86d
PrimeAnalytics <support@primeanalytics.io>
2016-05-23 20:35:31 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='SyncAlot',
+    version='',
+    packages=['SyncAlot', 'SyncAlot.Api', 'SyncAlot.Helpers', 'SyncAlot.Products', 'SyncAlot.Scrapers',
+              'SyncAlot.migrations', 'SyncAlot.Product_Url'],
+    url='',
+    license='',
+    author='enricowillemse',
+    author_email='',
+    description=''
+)
